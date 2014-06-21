b1e5a7a45125178f56f5c06068867931ac915056
Arran Jacques <arranjacques@gmail.com>
2014-06-21 13:51:01 +0100

    Monal blog package intial commit.

---
+++ src/Monal/Blog/BlogServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Monal\Blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('monal/blog');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
